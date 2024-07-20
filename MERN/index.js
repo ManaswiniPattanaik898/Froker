2e58525c35d3b3a50e30e80dc43c7a80aea810e7
manaswini pattanaik <133735641+ManaswiniPattanaik898@users.noreply.github.com>
2024-07-20 22:58:20 +0530

    Add files via upload

--- MERN/index.js
+++ MERN/index.js
@@ -1,1 +1,11 @@
+const navLinks = document.querySelectorAll('nav ul li a');
 
+navLinks.forEach(link => {
+  link.addEventListener('mouseover', () => {
+    link.style.color = '#ccc';
+  });
+
+  link.addEventListener('mouseout', () => {
+    link.style.color = 'black';
+  });
+});
